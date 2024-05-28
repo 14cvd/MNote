c0615e03c65fef7523784ed720d96bc38fb09108
14cvd <abbas3liyev@gmail.com>
2024-05-28 20:23:10 +0400

    Initial Commit

---
+++ MNotesUITests/MNotesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  MNotesUITestsLaunchTests.swift
+//  MNotesUITests
+//
+//  Created by Cavid on 28.05.24.
+//
+
+import XCTest
+
+final class MNotesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
