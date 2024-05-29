2c94be9350c69724b9da77c7a6dd16951e7310e6
Cavid Abbasaliyev <cavid.abbasaliyev@kapitalbank.az>
2024-05-29 09:49:10 +0400

    fix core service

--- MNotes/Data/Managers/CoreDataManager.swift
+++ MNotes/Data/Managers/CoreDataManager.swift
@@ -6,3 +6,27 @@
 //
 
 import Foundation
+import CoreData
+
+final class CoreDataManager {
+    static let shared = CoreDataManager(modelName : "Note")
+    private let persistentContainer : NSPersistentContainer
+    var context : NSManagedObjectContext {
+        return persistentContainer.viewContext
+    }
+    
+    
+    private init (modelName : String){
+        persistentContainer = NSPersistentContainer(name: modelName)
+    }
+    
+    func loadStore (completion : (() -> Void)? = nil){
+        persistentContainer.loadPersistentStores{storeDescription,erorr in
+            
+            if let error = erorr as? NSError {
+                fatalError("Core Data store failed to load: \(error)")
+                
+            }
+        }
+    }
+}
